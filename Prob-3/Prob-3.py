dd71d732b47c8d760ccc07abdd9e76ef92d33b55
Bill Baker <bill@baker88.com>
2019-05-17 14:55:33 -0700

    Initial version

---
+++ Prob-3/Prob-3.py
@@ -0,0 +1,14 @@
+# Module 7
+#   Programming Assignment 10
+#     Prob-3.py
+
+# <YOUR NAME>
+
+def main():
+    # your code here
+
+    # do not change the while loop definition below
+    while True:
+        # your code here
+
+main()    
---
+++ Prob-5-Bonus/Prob-5.py
@@ -0,0 +1,10 @@
+# Module 7
+#   Programming Assignment 10
+#     Prob-5.py
+
+# <YOUR NAME>
+
+def main():
+    # your code here
+
+main()    
